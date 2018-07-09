c39e4b30b532b5e05b9cc1d4fd94bbfcf8efed5a
jamie.pennell <jamie.pennell@cloudcall.com>
2018-07-09 09:40:53 +0100

    interface update

--- src/services/jira/i-jira.service.ts
+++ src/services/jira/i-jira.service.ts
@@ -1,7 +1,9 @@
 import { IUserData } from "../../interfaces/i-user-data";
 import { IBoard } from "../../interfaces/i-board";
+import { ISprint } from "../../interfaces/i-sprint";
 
 export interface IJiraService {
     getBoards(userDetails: IUserData): Promise<IBoard[]>;
     getBoard(userDetails: IUserData, boardId: number): Promise<IBoard>;
+    getSprint(userDetails: IUserData, sprintId: number): Promise<ISprint>;
 }
